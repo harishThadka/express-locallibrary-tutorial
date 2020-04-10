597f8514c047e3e7218c107331166357bae6b1b0
harish24680 <S538362@nwmissouri.edu>
2020-04-10 02:18:05 -0500

    Commit mongodb

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+//Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const mongoDB = 'mongodb+srv://S538362:Hari@95736@cluster0-qhch7.azure.mongodb.net/test?retryWrites=true&w=majority';
+// mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, {
+useNewUrlParser: true,useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
