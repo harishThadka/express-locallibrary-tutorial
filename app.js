b2754d59f4edbfbc9f4d7ccf2282dc9d9b2ae084
harish24680 <S538362@nwmissouri.edu>
2020-05-01 14:08:21 -0500

    commit

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const createError = require('http-errors');
 const express = require('express');
-const dotenv = require('dotenv');
-
-
-
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');
+const catalogRouter = require('./routes/catalog'); 
+const compression = require('compression');
+const dotenv = require('dotenv')
+const helmet = require('helmet');
 
+//setting up database
 const app = express();
-dotenv.config({path: '.env'});
-//Set up mongoose connection
+
+dotenv.config({ path: '.env' })
 const mongoose = require('mongoose');
+
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
-const mongoDB = process.env.ATLAS_URI;
-mongoose.connect(mongoDB, {
-useNewUrlParser: true,useUnifiedTopology: true
-});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -28,15 +27,18 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
